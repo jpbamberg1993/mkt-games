d46e918236e0bbcc8f334d526117d01fa90fc625
Paul Bamberg <jpbamberg1993@gmail.com>
2023-12-09 16:30:08 -0500

    Create match title

--- src/app/match-game/layout.tsx
+++ src/app/match-game/layout.tsx
@@ -6,8 +6,8 @@ export default function MatchGameLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<div className="bg-custom container mx-auto min-h-screen bg-[url('/match-background-gradient.webp')] bg-cover bg-no-repeat">
-			<div className='absolute inset-0 bg-black bg-opacity-40'></div>
+		<div className="bg-custom container relative mx-auto min-h-screen bg-[url('/match-background-gradient.webp')] bg-cover bg-no-repeat">
+			<div className='absolute inset-0 z-10 bg-black bg-opacity-40'></div>
 			{children}
 		</div>
 	)
--- src/app/match-game/page.tsx
+++ src/app/match-game/page.tsx
@@ -1,3 +1,18 @@
+import Image from 'next/image'
+
 export default function MatchGame() {
-	return <div>Match Game</div>
+	return (
+		<div className='relative z-20 pt-24 text-center'>
+			<Image
+				src='/marlboro-vista-logo.svg'
+				alt='Marlboro Vista Logo'
+				width={700}
+				height={300}
+				className='mx-auto'
+			/>
+			<h2 className='mx-auto max-w-lg pt-11 text-3xl text-white'>
+				¡Conecta los sabores correctos con sus cajetillas!
+			</h2>
+		</div>
+	)
 }
