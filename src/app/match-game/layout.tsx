0cd1aff8e7c4ab4e8f1d47db00a6a3329e6bf821
Paul Bamberg <jpbamberg1993@gmail.com>
2023-12-10 17:40:44 -0500

    Create warning footer

--- src/app/match-game/layout.tsx
+++ src/app/match-game/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { WarningFooter } from '@/components/warning-footer'
 
 export default function MatchGameLayout({
 	children,
@@ -9,6 +10,7 @@ export default function MatchGameLayout({
 		<div className="bg-custom container relative mx-auto min-h-screen bg-[url('/match-background-gradient.webp')] bg-cover bg-no-repeat">
 			<div className='absolute inset-0 z-10 bg-black bg-opacity-40'></div>
 			{children}
+			<WarningFooter />
 		</div>
 	)
 }
---
+++ src/components/warning-footer.tsx
@@ -0,0 +1,12 @@
+export function WarningFooter() {
+	return (
+		<div className='fixed inset-x-0 bottom-0 z-20 text-center'>
+			<div className='flex h-28 items-center justify-center bg-white'>
+				<h4 className='text-3xl'>PROHIBIDA SU VENTA A MENORES DE 18 AÑOS</h4>
+			</div>
+			<div className='flex h-32 items-center justify-center bg-black text-white'>
+				<h4 className='text-4xl'>FUMAR CAUSA CÁNCER DE PULMÓN</h4>
+			</div>
+		</div>
+	)
+}
