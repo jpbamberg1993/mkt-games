93dd3c6236baebb87c721110e66f75f2e0f868ab
Paul Bamberg <jpbamberg1993@gmail.com>
2023-12-12 08:02:36 -0500

    Improve font of footer messages

--- src/components/warning-footer.tsx
+++ src/components/warning-footer.tsx
@@ -2,10 +2,14 @@ export function WarningFooter() {
 	return (
 		<div className='fixed inset-x-0 bottom-0 z-20 text-center'>
 			<div className='flex h-28 items-center justify-center bg-white'>
-				<h4 className='text-3xl'>PROHIBIDA SU VENTA A MENORES DE 18 AÑOS</h4>
+				<h4 className='font-sans text-3xl font-bold'>
+					PROHIBIDA SU VENTA A MENORES DE 18 AÑOS
+				</h4>
 			</div>
 			<div className='flex h-32 items-center justify-center bg-black text-white'>
-				<h4 className='text-4xl'>FUMAR CAUSA CÁNCER DE PULMÓN</h4>
+				<h4 className='font-sans text-4xl font-bold'>
+					FUMAR CAUSA CÁNCER DE PULMÓN
+				</h4>
 			</div>
 		</div>
 	)
